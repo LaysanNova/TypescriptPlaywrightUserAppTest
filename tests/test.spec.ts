77269ea4e5c6f0771796434e59aab8f82535767c
Laysan Nova <laysansh@gmail.com>
2025-01-19 08:18:02 -0700

    first commit

---
+++ tests/test.spec.ts
@@ -0,0 +1,10 @@
+import {expect, test} from "@playwright/test";
+
+test("first", async ({ page }) => {
+    await page.goto('https://nodeexpressapi-n9sc.onrender.com/');
+    expect(page.url()).toEqual('https://nodeexpressapi-n9sc.onrender.com/');
+})
+
+test("has title", async ({ page }) => {
+    await expect(await page.title()).toHaveTitle("Users app");
+})
