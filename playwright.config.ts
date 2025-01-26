be5b54e47bf28be30ca4290463ccbdbf7c0eb8f6
Laysan Nova <laysansh@gmail.com>
2025-01-26 10:37:15 -0700

    Add Allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import * as os from "node:os";
 
 /**
  * Read environment variables from file.
@@ -36,7 +37,44 @@ export default defineConfig({
     ['html', { outputFolder: 'reports/html-report/', open: 'never' }],
     ['junit', { outputFile: 'reports/junit-report/test-results.xml' }],
     ['@estruyf/github-actions-reporter'],
-    ['monocart-reporter', { name: "Monocart Report", outputFile: 'reports/monocart-report/index.html' }]
+    ['monocart-reporter', { name: "Monocart Report", outputFile: 'reports/monocart-report/index.html' }],
+    [
+      "allure-playwright",
+      {
+        resultsDir: "reports/allure-report",
+        detail: true,
+        suiteTitle: true,
+        // links: {
+        //   issue: {
+        //     nameTemplate: "Issue #%s",
+        //     urlTemplate: "https://issues.example.com/%s",
+        //   },
+        //   tms: {
+        //     nameTemplate: "TMS #%s",
+        //     urlTemplate: "https://tms.example.com/%s",
+        //   },
+        //   jira: {
+        //     urlTemplate: (v) => `https://jira.example.com/browse/${v}`,
+        //   },
+        // },
+        categories: [
+          {
+            name: "Reports",
+            messageRegex: "bar",
+            traceRegex: "baz",
+            //matchedStatuses: [Status.FAILED, Status.BROKEN],
+          },
+        ],
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+          process_platform: process.platform,
+        },
+        storeTrend: true
+      },
+    ],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -44,7 +82,11 @@ export default defineConfig({
     baseURL: 'https://nodeexpressapi-n9sc.onrender.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    headless: true,
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    ignoreHTTPSErrors: true,
   },
 
   /* Configure projects for major browsers */
--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -16,6 +16,7 @@ import { test, expect } from "@playwright/test";
 
             await expect(tab).toBeAttached();
             await expect(tab).toHaveCount(1);
+            await expect(tab).toHaveCount(2);
             await expect(tab).toBeVisible();
             await expect(tab).toBeEnabled();
             expect(tabClassAttribute).toStrictEqual(`${expected}`);
